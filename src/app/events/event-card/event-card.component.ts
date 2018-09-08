d85e1e90602ae42381552bfe81851067b18dfe60
Khuram Niaz <knshaikh@gmail.com>
2018-09-08 14:00:34 -0600

    display events

--- src/app/events/event-card/event-card.component.ts
+++ src/app/events/event-card/event-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
   selector: "app-event-card",
@@ -6,20 +6,9 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./event-card.component.css"]
 })
 export class EventCardComponent implements OnInit {
-  event: any = {
-    id: 1,
-    name: "Angular Connect",
-    date: "7/2/2019",
-    time: "10:00 am",
-    price: 400.0,
-    imageUrl: "/assets/images/angularconnect-shield.png",
-    location: {
-      address: "1057 DT",
-      city: "Calgary",
-      country: "Canada"
-    }
-  };
-  constructor() {}
+  @Input()
+  event: any;
 
+  constructor() {}
   ngOnInit() {}
 }
