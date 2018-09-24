e9a71ccdfad47e55e1c2c9fbeaaad913596f5af9
Khuram Niaz <knshaikh@gmail.com>
2018-09-24 12:25:00 -0600

    ghpages and demo link

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -58,7 +58,7 @@ const events: IEvent[] = [
     date: new Date("9/26/2036"),
     time: "10:00 am",
     price: 599.99,
-    imageUrl: "/assets/images/angularconnect-shield.png",
+    imageUrl: "assets/images/angularconnect-shield.png",
     location: {
       address: "123 XYZ",
       city: "Calgary",
@@ -136,7 +136,7 @@ const events: IEvent[] = [
     date: new Date("4/15/2037"),
     time: "9:00 am",
     price: 950.0,
-    imageUrl: "/assets/images/ng-nl.png",
+    imageUrl: "assets/images/ng-nl.png",
     location: {
       address: "The NG-NL Convention Center & Scuba Shop",
       city: "Amsterdam",
@@ -196,7 +196,7 @@ const events: IEvent[] = [
     date: new Date("5/4/2037"),
     time: "9:00 am",
     price: 759.0,
-    imageUrl: "/assets/images/ng-conf.png",
+    imageUrl: "assets/images/ng-conf.png",
     location: {
       address: "The Palatial America Hotel",
       city: "Salt Lake City",
@@ -278,7 +278,7 @@ const events: IEvent[] = [
     date: new Date("6/10/2037"),
     time: "8:00 am",
     price: 800.0,
-    imageUrl: "/assets/images/basic-shield.png",
+    imageUrl: "assets/images/basic-shield.png",
     location: {
       address: "The UN Angular Center",
       city: "New York",
@@ -327,7 +327,7 @@ const events: IEvent[] = [
     date: new Date("2/10/2037"),
     time: "9:00 am",
     price: 400.0,
-    imageUrl: "/assets/images/ng-vegas.png",
+    imageUrl: "assets/images/ng-vegas.png",
     location: {
       address: "The Excalibur",
       city: "Las Vegas",
